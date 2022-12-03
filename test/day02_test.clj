4291a9b434162970bd4fd59837f19d2107839598
Tyler Kindy <tylerkindy@gmail.com>
2022-12-02 22:12:28 -0500

    Add failing test

--- test/day02_test.clj
+++ test/day02_test.clj
@@ -1,3 +1,11 @@
 (ns day02-test
   (:require day02
             [clojure.test :refer [deftest is]]))
+
+(def example [[:rock     :paper]
+              [:paper    :rock]
+              [:scissors :scissors]])
+
+(deftest expected-score
+  (is (= (day02/expected-score example)
+         15)))
