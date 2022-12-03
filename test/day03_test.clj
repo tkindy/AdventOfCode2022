bbe8102babab6ea7a481f944e3c56ffbef3af94d
Tyler Kindy <tylerkindy@gmail.com>
2022-12-03 00:12:49 -0500

    Add failing test

--- test/day03_test.clj
+++ test/day03_test.clj
@@ -1,3 +1,9 @@
 (ns day03-test
   (:require day03
             [clojure.test :refer [deftest is]]))
+
+(def example (day03/parse-input (slurp "examples/day03.txt")))
+
+(deftest split-item-priority-sum
+  (is (= (day03/split-item-priority-sum example)
+         157)))
