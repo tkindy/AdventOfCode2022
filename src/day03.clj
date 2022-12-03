4c11834c141a59006d9a3237d273d4de383cf8d2
Tyler Kindy <tylerkindy@gmail.com>
2022-12-03 00:12:39 -0500

    Parse input

--- src/day03.clj
+++ src/day03.clj
@@ -1,8 +1,24 @@
-(ns day03)
+(ns day03
+  (:require [clojure.string :as str]))
 
-(defn parse-input [input])
+(defn parse-compartment [contents]
+  (apply list contents))
+
+(defn parse-rucksack [line]
+  (let [len (count line)]
+    [(parse-compartment (subs line 0 (/ len 2)))
+     (parse-compartment (subs line (/ len 2)))]))
+
+(defn parse-input [input]
+  (->> (str/split-lines input)
+       (map parse-rucksack)))
 
 (defn read-input []
   (parse-input (slurp "inputs/day03.txt")))
 
-(defn -main [])
+(defn split-item-priority-sum [rucksacks]
+  0)
+
+(defn -main []
+  (let [rucksacks (read-input)]
+    (println "Part 1:" (split-item-priority-sum rucksacks))))
