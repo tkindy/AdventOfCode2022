a94bf55ab3efaa796141353031015ccaeb79da5b
Tyler Kindy <tylerkindy@gmail.com>
2022-12-03 00:21:34 -0500

    Implement part 1

--- src/day03.clj
+++ src/day03.clj
@@ -1,5 +1,6 @@
 (ns day03
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
 
 (defn parse-compartment [contents]
   (apply list contents))
@@ -16,8 +17,25 @@
 (defn read-input []
   (parse-input (slurp "inputs/day03.txt")))
 
+(defn find-split-item [[left right]]
+  (-> (set/intersection (set left)
+                        (set right))
+      first))
+
+(def lower-priorities (->> (range (int \a) (inc (int \z)))
+                           (map (fn [i] [(char i) (+ (- i (int \a)) 1)]))
+                           (into {})))
+(def upper-priorities (->> (range (int \A) (inc (int \Z)))
+                           (map (fn [i] [(char i) (+ (- i (int \A)) 27)]))
+                           (into {})))
+
+(def priorities (merge lower-priorities upper-priorities))
+
 (defn split-item-priority-sum [rucksacks]
-  0)
+  (->> rucksacks
+       (map find-split-item)
+       (map priorities)
+       (apply +)))
 
 (defn -main []
   (let [rucksacks (read-input)]
