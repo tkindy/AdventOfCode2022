97681d10b415f805c4b19132b16fca6235116857
Tyler Kindy <tylerkindy@gmail.com>
2022-12-02 21:50:51 -0500

    Write failing test

--- src/day01.clj
+++ src/day01.clj
@@ -1,1 +1,14 @@
 (ns day01)
+
+(defn parse-input [input]
+  [])
+
+(defn read-input []
+  (parse-input (slurp "inputs/day01.txt")))
+
+(defn most-calories [elves]
+  0)
+
+(defn -main []
+  (let [elves (read-input)]
+    (println "Part 1:" (most-calories elves))))
--- test/day01_test.clj
+++ test/day01_test.clj
@@ -1,1 +1,11 @@
-(ns day01-test)
+(ns day01-test
+  (:require day01
+            [clojure.test :refer [deftest is]]))
+
+(deftest most-calories
+  (is (= (day01/most-calories [[1000 2000 3000]
+                               [4000]
+                               [5000 6000]
+                               [7000 8000 9000]
+                               [10000]])
+         24000)))
