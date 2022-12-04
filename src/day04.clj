fbe03caa5fbeb00ed525b6299620b21ee394ab95
Tyler Kindy <tylerkindy@gmail.com>
2022-12-04 00:11:30 -0500

    Finish part 1

--- src/day04.clj
+++ src/day04.clj
@@ -1,12 +1,13 @@
 (ns day04
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
 
 (defn parse-assignment [assignment]
-  (let [[start end] (str/split assignment "-")]
+  (let [[start end] (str/split assignment #"-")]
     [(parse-long start) (inc (parse-long end))]))
 
 (defn parse-pair [pair]
-  (let [[p1 p2] (str/split pair ",")]
+  (let [[p1 p2] (str/split pair #",")]
     [(parse-assignment p1) (parse-assignment p2)]))
 
 (defn parse-input [input]
@@ -15,8 +16,19 @@
 (defn read-input []
   (parse-input (slurp "inputs/day04.txt")))
 
+(defn assignment-set [[start end]]
+  (set (range start end)))
+
+(defn has-subset [[left right]]
+  (let [left (assignment-set left)
+        right (assignment-set right)]
+    (or (set/subset? left right)
+        (set/subset? right left))))
+
 (defn subset-count [pairs]
-  (throw (RuntimeException. "Not yet implemented")))
+  (->> pairs
+       (filter has-subset)
+       count))
 
 (defn -main []
   (let [pairs (read-input)]
