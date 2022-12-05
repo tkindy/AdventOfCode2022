ce6ceeb6a8d41d0bbab26a613d2150861141f29a
Tyler Kindy <tylerkindy@gmail.com>
2022-12-05 08:52:40 -0500

    Add parse-input test

--- test/day05_test.clj
+++ test/day05_test.clj
@@ -2,7 +2,17 @@
   (:require day05
             [clojure.test :refer [deftest is]]))
 
-(def example (day05/parse-input (slurp "examples/day05.txt")))
+(def example {:start {1 '(\N \Z)
+                      2 '(\D \C \M)
+                      3 '(\P)}
+              :procedure '({:count 1, :from 2, :to 1}
+                           {:count 3, :from 1, :to 3}
+                           {:count 2, :from 2, :to 1}
+                           {:count 1, :from 1, :to 2})})
+
+(deftest parse-input
+  (is (= (day05/parse-input (slurp "examples/day05.txt"))
+         example)))
 
 (deftest end-tops
   (is (= (day05/end-tops example)
