e91e9400487c73e6d39cd2c988efa64bb2eb7ca2
Tyler Kindy <tylerkindy@gmail.com>
2022-12-05 09:40:26 -0500

    Implement parsing

--- src/day05.clj
+++ src/day05.clj
@@ -1,7 +1,49 @@
-(ns day05)
+(ns day05
+  (:require [clojure.string :as str]))
+
+(defn parse-spot [i spot]
+  (let [letter (nth spot 1)]
+    (if (= letter \space)
+      {}
+      {(inc i) (list letter)})))
+
+(defn parse-level [line]
+  (->> line
+       (partition 4 4 [])
+       (map-indexed parse-spot)
+       (apply merge)))
+
+(defn parse-levels [lines]
+  (map parse-level lines))
+
+;; if the last stacks are empty, the map won't have their keys.
+;; ignoring that because my input doesn't have that problem
+(defn parse-start [start]
+  (->> (str/split-lines start)
+       drop-last
+       parse-levels
+       (apply merge-with concat)))
+
+(def step-regex #"move (\d+) from (\d+) to (\d+)")
+
+(defn parse-step [step]
+  (let [[_ count from to] (re-matches step-regex step)]
+    {:count (parse-long count)
+     :from (parse-long from)
+     :to (parse-long to)}))
+
+(re-matches step-regex "move 1 from 2 to 1")
+
+(defn parse-procedure [procedure]
+  (->> procedure
+       str/trim
+       str/split-lines
+       (map parse-step)))
 
 (defn parse-input [input]
-  (throw (RuntimeException. "Not yet implemented")))
+  (let [[start procedure] (str/split input #"\n\n")]
+    {:start (parse-start start)
+     :procedure (parse-procedure procedure)}))
 
 (defn read-input []
   (parse-input (slurp "inputs/day05.txt")))
--- test/day05_test.clj
+++ test/day05_test.clj
@@ -10,6 +10,20 @@
                            {:count 2, :from 2, :to 1}
                            {:count 1, :from 1, :to 2})})
 
+(deftest parse-spot
+  (is (= (day05/parse-spot 0 "    ")
+         {}))
+  (is (= (day05/parse-spot 1 "[D]")
+         {2 [\D]})))
+
+(deftest parse-level
+  (is (= (day05/parse-level "    [D]")
+         {2 [\D]})))
+
+(deftest parse-step
+  (is (= (day05/parse-step "move 1 from 2 to 1")
+         {:count 1, :from 2, :to 1})))
+
 (deftest parse-input
   (is (= (day05/parse-input (slurp "examples/day05.txt"))
          example)))
