397a8b4da0c27448ee5ad69505da9e747cb2951c
Tyler Kindy <tylerkindy@gmail.com>
2022-12-07 08:50:03 -0500

    Take first pass at parsing

--- src/day07.clj
+++ src/day07.clj
@@ -1,7 +1,72 @@
-(ns day07)
+(ns day07
+  (:require [clojure.string :as str]))
+
+(defn parse-command-line [line]
+  (let [[_ command arg] (str/split line #" ")]
+    [(keyword command) arg]))
+
+(defn split-execution [lines]
+  (let [[command arg] (parse-command-line (first lines))
+        output (->> (rest lines)
+                    (take-while #(not (str/starts-with? % "$"))))]
+    [{:command command
+      :arg arg
+      :output output}
+     (drop (inc (count output)) lines)]))
+
+(defn split-executions [lines]
+  (let [[execution rest] (split-execution lines)]
+    (cons execution (split-executions rest))))
+
+(defn split-path [path]
+  (rest (str/split path #"/")))
+
+(defn cd-up [path]
+  (str "/" (->> path
+                split-path
+                drop-last
+                (str/join "/"))))
+
+(defn change-directory [{:keys [path] :as state} {:keys [arg]}]
+  (let [new-path (case arg
+                   "/" "/"
+                   ".." (cd-up path)
+                   (str path "/" arg))]
+    (assoc state :path new-path)))
+
+(defn parse-file [line]
+  (let [[size name] (str/split line #" ")]
+    [name {:size (parse-long size)}]))
+
+(defn parse-files [output]
+  (->> output
+       (filter #(not (str/starts-with? % "dir")))
+       (map parse-file)
+       (into {})))
+
+(defn path->keys [path]
+  (mapcat (fn [key] [:contents key])
+          (split-path path)))
+
+(defn capture-files [{:keys [path root]} {:keys [output]}]
+  (let [files (parse-files output)]
+    (update-in root
+               (concat (path->keys path) [:contents])
+               merge files)))
+
+(defn process-execution [state {:keys [command] :as execution}]
+  (case command
+    :cd (change-directory state execution)
+    :ls (capture-files state execution)))
 
 (defn parse-input [input]
-  (throw (RuntimeException. "Not yet implemented")))
+  (->> input
+       str/trim
+       str/split-lines
+       split-executions
+       (reduce process-execution {:path "/"
+                                  :root {:contents {}}})
+       :root))
 
 (defn read-input []
   (parse-input (slurp "inputs/day07.txt")))
