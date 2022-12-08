bbd5075010386f316097a1b23abcb0deef1a08c8
Tyler Kindy <tylerkindy@gmail.com>
2022-12-08 08:12:41 -0500

    Check for edges

--- src/day08.clj
+++ src/day08.clj
@@ -16,13 +16,30 @@
 (defn grid->seq [grid]
   (for [y (range (count grid))
         x (range (count (first grid)))]
-    [[x y] (get-in grid [y x])]))
+    {:x x, :y y, :height (get-in grid [y x])}))
 
-(defn num-visible [grid]
+(defn on-edge? [[x y] grid]
+  (or (= x 0)
+      (= x (dec (count (first grid))))
+      (= y 0)
+      (= y (dec (count grid)))))
+
+(defn check-spot [visible
+                  {:keys [x y height] :as spot}
+                  grid]
+  (if (on-edge? [x y] grid)
+    (conj visible [x y])
+    visible))
+
+(defn find-visible [grid]
   (->> grid
        grid->seq
-       (filter (fn [tree] (visible? tree grid)))
-       count))
+       (reduce (fn [visible spot]
+                 (check-spot visible spot grid))
+               #{})))
+
+(defn num-visible [grid]
+  (count (find-visible grid)))
 
 (defn -main []
   (let [grid (read-input)]
