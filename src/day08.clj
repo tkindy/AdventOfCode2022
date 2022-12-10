64db3a2f18f05d4e6f8f1235bd4115863698a57b
Tyler Kindy <tylerkindy@gmail.com>
2022-12-10 14:09:58 -0500

    Refinish part 1

--- src/day08.clj
+++ src/day08.clj
@@ -19,8 +19,29 @@
         x (range (count (first grid)))]
     {:x x, :y y, :height (get-in grid [y x])}))
 
+(defn visible-in-dir? [{:keys [height]} line]
+  (or (empty? line)
+      (< (apply max line) height)))
+
+(defn extract-row [grid row start end]
+  (subvec (nth grid row) start end))
+
+(defn extract-column [grid col start end]
+  (->> grid
+       (mapv (fn [row] (nth row col)))
+       (drop start)
+       (take (- end start))))
+
+(defn visible? [{:keys [x y] :as spot} grid]
+  (or (visible-in-dir? spot (extract-row grid y 0 x))
+      (visible-in-dir? spot (extract-row grid y (inc x) (count grid)))
+      (visible-in-dir? spot (extract-column grid x 0 y))
+      (visible-in-dir? spot (extract-column grid x (inc y) (count grid)))))
+
 (defn find-visible [grid]
-  #{})
+  (->> grid
+       grid->seq
+       (filter (fn [spot] (visible? spot grid)))))
 
 (defn num-visible [grid]
   (count (find-visible grid)))
