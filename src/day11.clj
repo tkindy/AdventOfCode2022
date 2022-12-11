d0da1900c1d345c23f272d8f151e17f1c64fd78e
Tyler Kindy <tylerkindy@gmail.com>
2022-12-11 07:31:23 -0500

    Set up part 1

---
+++ src/day11.clj
@@ -0,0 +1,14 @@
+(ns day11)
+
+(defn parse-input [input]
+  (throw (RuntimeException. "Not yet implemented")))
+
+(defn read-input []
+  (parse-input (slurp "inputs/day11.txt")))
+
+(defn monkey-business-20 [monkeys]
+  (throw (RuntimeException. "Not yet implemented")))
+
+(defn -main []
+  (let [monkeys (read-input)]
+    (println "Part 1:" (monkey-business-20 monkeys))))
---
+++ test/day11_test.clj
@@ -0,0 +1,33 @@
+(ns day11-test
+  (:require day11
+            [clojure.test :refer [deftest is]]))
+
+(def example
+  [{:items [79 98]
+    :operation '(* old 19)
+    :test {:divisor 23
+           true 2
+           false 3}}
+   {:items [54 65 75 74]
+    :operation '(+ old 6)
+    :test {:divisor 19
+           true 2
+           false 0}}
+   {:items [79 60 97]
+    :operation '(* old old)
+    :test {:divisor 13
+           :true 1
+           :false 3}}
+   {:items [74]
+    :operation '(+ old 3)
+    :test {:divisor 17
+           true 0
+           false 1}}])
+
+(deftest parse-input
+  (is (= (day11/parse-input (slurp "examples/day11.txt"))
+         example)))
+
+(deftest monkey-business-20
+  (is (= (day11/monkey-business-20 example)
+         10605)))
