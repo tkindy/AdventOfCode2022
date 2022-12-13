6b707b283cc036271762abad6ee8267874e48cb1
Tyler Kindy <tylerkindy@gmail.com>
2022-12-12 22:52:13 -0500

    Finish part 1

--- src/day12.clj
+++ src/day12.clj
@@ -47,8 +47,78 @@
 (defn read-input []
   (parse-input (slurp "inputs/day12.txt")))
 
+(defn build-unvisited [heightmap]
+  (set (grid->coords heightmap)))
+
+(defn build-distances [{:keys [start heightmap]}]
+  (assoc (->> heightmap
+              grid->coords
+              (map (fn [node] [node {:distance ##Inf}]))
+              (into {}))
+         start
+         {:distance 0}))
+
+(defn get-height [{:keys [x y]} heightmap]
+  (get-in heightmap [y x]))
+
+(defn legal-neighbor? [candidate height heightmap]
+  (let [candidate-height (get-height candidate heightmap)]
+    (and candidate-height
+         (<= candidate-height (inc height)))))
+
+(defn neighbors [{:keys [x y] :as node} heightmap]
+  (let [height (get-height node heightmap)
+        candidates [{:x x, :y (dec y)}
+                    {:x x, :y (inc y)}
+                    {:x (dec x), :y y}
+                    {:x (inc x), :y y}]]
+    (filter (fn [candidate]
+              (legal-neighbor? candidate height heightmap))
+            candidates)))
+
+(defn unvisited-neighbors [current heightmap unvisited]
+  (->> (neighbors current heightmap)
+       (filter unvisited)))
+
+(defn update-distances [current neighbors distances]
+  (reduce (fn [distances neighbor]
+            (let [current-distance (:distance (distances current))
+                  neighbor-distance (inc current-distance)]
+              (if (< neighbor-distance (:distance (distances neighbor)))
+                (assoc distances neighbor {:distance neighbor-distance
+                                           :from current})
+                distances)))
+          distances
+          neighbors))
+
+(defn build-path [distances start end]
+  (loop [current end, path '()]
+    (let [path (conj path current)]
+      (if (= current start)
+        path
+        (recur (:from (distances current))
+               path)))))
+
+(defn next-node [unvisited distances]
+  (->> unvisited
+       (apply min-key (fn [node] (:distance (distances node))))))
+
+(defn shortest-path [{:keys [start end heightmap] :as state}]
+  (loop [unvisited (build-unvisited heightmap)
+         distances (build-distances state)
+         current   start]
+    (let [neighbors (unvisited-neighbors current heightmap unvisited)
+          distances (update-distances current neighbors distances)
+          unvisited (disj unvisited current)]
+      (if (= current end)
+        (build-path distances start end)
+        (recur unvisited distances (next-node unvisited distances))))))
+
 (defn shortest-path-distance [state]
-  (throw (RuntimeException. "Not yet implemented")))
+  (-> state
+      shortest-path
+      count
+      dec))
 
 (defn -main []
   (let [state (read-input)]
--- test/day12_test.clj
+++ test/day12_test.clj
@@ -15,6 +15,19 @@
   (is (= (day12/parse-input (slurp "examples/day12.txt"))
          example)))
 
+(deftest build-unvisited
+  (is (= (day12/build-unvisited (:heightmap example))
+         (set [{:x 0, :y 0} {:x 1, :y 0} {:x 2, :y 0} {:x 3, :y 0}
+               {:x 4, :y 0} {:x 5, :y 0} {:x 6, :y 0} {:x 7, :y 0}
+               {:x 0, :y 1} {:x 1, :y 1} {:x 2, :y 1} {:x 3, :y 1}
+               {:x 4, :y 1} {:x 5, :y 1} {:x 6, :y 1} {:x 7, :y 1}
+               {:x 0, :y 2} {:x 1, :y 2} {:x 2, :y 2} {:x 3, :y 2}
+               {:x 4, :y 2} {:x 5, :y 2} {:x 6, :y 2} {:x 7, :y 2}
+               {:x 0, :y 3} {:x 1, :y 3} {:x 2, :y 3} {:x 3, :y 3}
+               {:x 4, :y 3} {:x 5, :y 3} {:x 6, :y 3} {:x 7, :y 3}
+               {:x 0, :y 4} {:x 1, :y 4} {:x 2, :y 4} {:x 3, :y 4}
+               {:x 4, :y 4} {:x 5, :y 4} {:x 6, :y 4} {:x 7, :y 4}]))))
+
 (deftest shortest-path-distance
   (is (= (day12/shortest-path-distance example)
          31)))
