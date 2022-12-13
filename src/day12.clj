eb25b2a9f4d2712dbac8795b3733349994911ddd
Tyler Kindy <tylerkindy@gmail.com>
2022-12-12 22:27:31 -0500

    Parse out start and end

--- src/day12.clj
+++ src/day12.clj
@@ -1,6 +1,31 @@
 (ns day12
   (:require [clojure.string :as str]))
 
+(defn parse-grid [input]
+  (->> input
+       str/trim
+       str/split-lines
+       (mapv identity)))
+
+(defn grid->coords [grid]
+  (for [y (range (count grid))
+        x (range (count (first grid)))]
+    {:x x, :y y}))
+
+(defn start [grid]
+  (->> grid
+       grid->coords
+       (filter (fn [{:keys [x y]}]
+                 (= (get-in grid [y x]) \S)))
+       first))
+
+(defn end [grid]
+  (->> grid
+       grid->coords
+       (filter (fn [{:keys [x y]}]
+                 (= (get-in grid [y x]) \E)))
+       first))
+
 (defn parse-height [c]
   (case c
     \S 0
@@ -10,18 +35,21 @@
 (defn parse-line [line]
   (mapv parse-height line))
 
+(defn heightmap [grid]
+  (mapv parse-line grid))
+
 (defn parse-input [input]
-  (->> input
-       str/trim
-       str/split-lines
-       (mapv parse-line)))
+  (let [grid (parse-grid input)]
+    {:start (start grid)
+     :end (end grid)
+     :heightmap (heightmap grid)}))
 
 (defn read-input []
   (parse-input (slurp "inputs/day12.txt")))
 
-(defn shortest-path-distance [heightmap]
+(defn shortest-path-distance [state]
   (throw (RuntimeException. "Not yet implemented")))
 
 (defn -main []
-  (let [heightmap (read-input)]
-    (println "Part 1:" (shortest-path-distance heightmap))))
+  (let [state (read-input)]
+    (println "Part 1:" (shortest-path-distance state))))
--- test/day12_test.clj
+++ test/day12_test.clj
@@ -2,11 +2,14 @@
   (:require day12
             [clojure.test :refer [deftest is]]))
 
-(def example [[0  0  1 16 15 14 13 12]
-              [0  1  2 17 24 23 23 11]
-              [0  2  2 18 25 25 23 10]
-              [0  2  2 19 20 21 22  9]
-              [0  1  3  4  5  6  7  8]])
+(def example {:start {:x 0, :y 0}
+              :end {:x 5, :y 2}
+              :heightmap
+              [[0  0  1 16 15 14 13 12]
+               [0  1  2 17 24 23 23 11]
+               [0  2  2 18 25 25 23 10]
+               [0  2  2 19 20 21 22  9]
+               [0  1  3  4  5  6  7  8]]})
 
 (deftest parse-input
   (is (= (day12/parse-input (slurp "examples/day12.txt"))
